81307a20e69334e542ff481e04d59110d7d56e18
artemchepeliuk <4epeliuk@gmail.com>
2019-06-20 12:56:50 +0300

    first commit

---
+++ precache-manifest.eedfa377fd654ebd1c40361588725518.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9cfd0b7c5c111f73820d4cb1b1719a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4951db6d1e3530f139a",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3a8764acc284b08b7577",
+    "url": "/static/css/main.aae7c960.chunk.css"
+  },
+  {
+    "revision": "e4951db6d1e3530f139a",
+    "url": "/static/js/2.fc8d7157.chunk.js"
+  },
+  {
+    "revision": "3a8764acc284b08b7577",
+    "url": "/static/js/main.c729ad37.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e7055e8bb7c53b0da4a8f37b61a91e25",
+    "url": "/static/media/01d.e7055e8b.jpg"
+  },
+  {
+    "revision": "8bcf049ccc09414d01a56fd778b0ff59",
+    "url": "/static/media/02d.8bcf049c.jpg"
+  },
+  {
+    "revision": "dad529d17ba444e8e6800de7daca7de5",
+    "url": "/static/media/03d.dad529d1.jpg"
+  },
+  {
+    "revision": "438d523f9cc0eab1621e7231ccf0952a",
+    "url": "/static/media/04d.438d523f.png"
+  },
+  {
+    "revision": "2e04318d98d0e8d6366eb0c95753b474",
+    "url": "/static/media/10d.2e04318d.jpg"
+  },
+  {
+    "revision": "aa395a926491186ff36afbb19187f338",
+    "url": "/static/media/10n.aa395a92.jpg"
+  },
+  {
+    "revision": "057e995eed9bb36dc7c26e4d801e523f",
+    "url": "/static/media/50n.057e995e.jpg"
+  }
+]);
